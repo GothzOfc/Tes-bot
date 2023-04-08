f2a1dea63eb9f72777d50617e6ec241ea81c1055
GothzOfc <130225677+GothzOfc@users.noreply.github.com>
2023-04-08 19:44:05 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,292 @@
+// disini atur aja kak menu bot nya
+// jangan lupa hapus fitur yg ga penting
+
+var monoSpace = '```'
+
+exports.listmenu = (sender,prefix,ad,namenya,premnya,usernya,romnya,tanggal,jam,no) => {
+return`
+♧︎︎︎════《𝙸𝙺𝙱𝙰𝙻 𝙱𝙾𝚃》════♧︎︎︎
+
+ ╔═══《 ᴜsᴇʀ ɪɴғᴏ 》════⊱
+ ╠➤ ɪᴅ : @${sender.split('@')[0]}
+ ╠➤ ɴᴀᴍᴀ : ${namenya}
+ ╠➤ ᴘʀᴇᴍɪᴜᴍ : ${premnya}
+ ╚═════════════⊱
+ ╔═══《 ʙᴏᴛ ɪɴғᴏ 》════⊱
+ ╠➤ ʟɪʙʀᴀʀʏ : Baileys-MD
+ ╠➤ ᴛɪᴍᴇ : ${jam} WIB
+ ╠➤ ᴅᴀᴛᴇ : ${tanggal}
+ ╠➤ ᴛᴇʀᴅᴀғᴛᴀʀ : ${usernya}
+ ╠➤ ʀᴏᴏᴍ ᴄʜᴀᴛ : ${romnya} 
+ ╚═════════════⊱
+ ╔═══《 ʙᴏᴛ ɪɴғᴏ 》════⊱
+ ╠͏ ʲⁱᵏᵃ ᵇᵒᵗ ᵗⁱᵇᵃ ᵗⁱᵇᵃ ᵐᵃᵗⁱ ˢⁱˡᵃᵏᵃⁿ ᵇⁱˡᵃⁿᵍ ᵏᵉ ᵒʷⁿᵉʳ
+ ╠ ⁿᵒᵐᵒʳ ᵒʷⁿᵉʳ ᵃᵈᵃ ᵈⁱ ᵖʳᵒᶠⁱˡᵉ ᵇᵒᵗⁿʸᵃ 
+ ╚═════════════⊱
+ 𝙈𝘼𝙄𝙉 𝙈𝙀𝙉𝙐
+ ${no++} ➳ ${prefix}menu
+ ${no++} ➳ ${prefix}rules
+ ${no++} ➳ ${prefix}owner
+ ${no++} ➳ ${prefix}infobot
+ ${no++} ➳ ${prefix}donasi
+ ${no++} ➳ ${prefix}donate
+ ${no++} ➳ ${prefix}sewabot
+ ${no++} ➳ ${prefix}groupbot
+ ${no++} ➳ ${prefix}ownerinfo
+ ${no++} ➳ ${prefix}infoowner
+
+ 𝙐𝙎𝙀𝙍 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}verify
+ ${no++} ➳ ${prefix}report
+ ${no++} ➳ ${prefix}request
+ ${no++} ➳ ${prefix}menfess
+ ${no++} ➳ ${prefix}buatroom
+ ${no++} ➳ ${prefix}secretchat
+ 
+ 𝙂𝙍𝙊𝙐𝙋 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}fitnah
+ ${no++} ➳ ${prefix}delete
+ ${no++} ➳ ${prefix}revoke
+ ${no++} ➳ ${prefix}tagall
+ ${no++} ➳ ${prefix}hidetag
+ ${no++} ➳ ${prefix}setdesc
+ ${no++} ➳ ${prefix}linkgrup
+ ${no++} ➳ ${prefix}infogroup
+ ${no++} ➳ ${prefix}setppgrup
+ ${no++} ➳ ${prefix}setnamegrup
+ ${no++} ➳ ${prefix}group open
+ ${no++} ➳ ${prefix}group close
+ ${no++} ➳ ${prefix}antilink on
+ ${no++} ➳ ${prefix}antilink off
+ ${no++} ➳ ${prefix}welcome on
+ ${no++} ➳ ${prefix}welcome off
+ ${no++} ➳ ${prefix}tiktokauto on
+ ${no++} ➳ ${prefix}tiktokauto off
+ ${no++} ➳ ${prefix}kick @tag
+ ${no++} ➳ ${prefix}demote @tag
+ ${no++} ➳ ${prefix}promote @tag
+ 
+ 𝘾𝙊𝙑𝙀𝙍𝙏 𝙈𝙀𝙉𝙐
+ ${no++} ➳ ${prefix}emojimix
+ ${no++} ➳ ${prefix}emojmix
+ ${no++} ➳ ${prefix}emojinua
+ ${no++} ➳ ${prefix}mixemoji
+ ${no++} ➳ ${prefix}stiker
+ ${no++} ➳ ${prefix}sticker
+ ${no++} ➳ ${prefix}sgif
+ ${no++} ➳ ${prefix}stikergif
+ ${no++} ➳ ${prefix}stickergif
+ ${no++} ➳ ${prefix}swm
+ ${no++} ➳ ${prefix}stikerwm
+ ${no++} ➳ ${prefix}stickerwm
+ ${no++} ➳ ${prefix}smeme
+ ${no++} ➳ ${prefix}memestiker
+ ${no++} ➳ ${prefix}stikermeme
+ ${no++} ➳ ${prefix}stickermeme
+ ${no++} ➳ ${prefix}takesticker
+ ${no++} ➳ ${prefix}emojinua2
+ ${no++} ➳ ${prefix}mixemoji2
+ ${no++} ➳ ${prefix}emojmix2
+ ${no++} ➳ ${prefix}emojimix2
+
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝘾𝙀𝙆 
+ ${no++} ➳ ${prefix}cekjelek
+ ${no++} ➳ ${prefix}cekcantik
+ ${no++} ➳ ${prefix}cekganteng
+ ${no++} ➳ ${prefix}ceksad
+ ${no++} ➳ ${prefix}cekharam
+ ${no++} ➳ ${prefix}cekhalal
+ ${no++} ➳ ${prefix}cekbego
+ ${no++} ➳ ${prefix}cekanjing
+ ${no++} ➳ ${prefix}cekbiadab
+ ${no++} ➳ ${prefix}cekramah
+ ${no++} ➳ ${prefix}ceksatir
+ ${no++} ➳ ${prefix}cekmanis
+ ${no++} ➳ ${prefix}cekpahit
+ ${no++} ➳ ${prefix}cekhitam
+ ${no++} ➳ ${prefix}cekrasis
+ ${no++} ➳ ${prefix}cekbaik
+ ${no++} ➳ ${prefix}cekjahat
+ ${no++} ➳ ${prefix}cekkaya
+ ${no++} ➳ ${prefix}cekmiskin
+ ${no++} ➳ ${prefix}cekpintar
+ ${no++} ➳ ${prefix}cekbodoh
+ ${no++} ➳ ${prefix}cekimut
+ ${no++} ➳ ${prefix}cekkocak
+ ${no++} ➳ ${prefix}cekkadang
+
+ 𝘼𝙉𝙊𝙉𝙔𝙈𝙊𝙐𝙎 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}buatroom 628xxx
+ ${no++} ➳ ${prefix}room <only owner>
+ ${no++} ➳ ${prefix}stopchat <only room>
+ ${no++} ➳ ${prefix}menfess 628xx|bot|hai
+
+ 𝙋𝙍𝙄𝙈𝘽𝙊𝙉 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}ramaljodoh
+ ${no++} ➳ ${prefix}ramalanjodoh
+ ${no++} ➳ ${prefix}nomorhoki
+ ${no++} ➳ ${prefix}artimimpi
+ ${no++} ➳ ${prefix}artinama
+ ${no++} ➳ ${prefix}sifatusaha
+ ${no++} ➳ ${prefix}tafsirmimpi
+ ${no++} ➳ ${prefix}pasangan
+ ${no++} ➳ ${prefix}suamiistri
+ ${no++} ➳ ${prefix}ramalcinta
+ ${no++} ➳ ${prefix}ramalancinta
+ ${no++} ➳ ${prefix}ramaljodohbali
+ ${no++} ➳ ${prefix}ramalanjodohbali
+ ${no++} ➳ ${prefix}cocoknama
+ ${no++} ➳ ${prefix}kecocokannama
+ ${no++} ➳ ${prefix}cocokpasangan
+ ${no++} ➳ ${prefix}kecocokanpasangan
+
+ 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}gempa
+ ${no++} ➳ ${prefix}jadwaltv
+ ${no++} ➳ ${prefix}gempanow
+ ${no++} ➳ ${prefix}bioskopnow
+ ${no++} ➳ ${prefix}latintoaksara
+ ${no++} ➳ ${prefix}aksaratolatin
+
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 
+ ${no++} ➳ ${prefix}dadu
+ ${no++} ➳ ${prefix}anjing
+ ${no++} ➳ ${prefix}patrick
+ ${no++} ➳ ${prefix}bucinstick
+ ${no++} ➳ ${prefix}gawrgura
+ ${no++} ➳ ${prefix}amongus
+ 
+ 𝙁𝙐𝙉 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}goblokcek 
+ ${no++} ➳ ${prefix}jelekcek 
+ ${no++} ➳ ${prefix}gaycek
+ ${no++} ➳ ${prefix}lesbicek
+ ${no++} ➳ ${prefix}gantengcek 
+ ${no++} ➳ ${prefix}cantikcek
+ ${no++} ➳ ${prefix}begocek 
+ ${no++} ➳ ${prefix}suhucek
+ ${no++} ➳ ${prefix}pintercek
+ ${no++} ➳ ${prefix}jagocek
+ ${no++} ➳ ${prefix}nolepcek
+ ${no++} ➳ ${prefix}babicek
+ ${no++} ➳ ${prefix}bebancek
+ ${no++} ➳ ${prefix}baikcek
+ ${no++} ➳ ${prefix}jahatcek
+ ${no++} ➳ ${prefix}anjingcek
+ ${no++} ➳ ${prefix}haramcek
+ ${no++} ➳ ${prefix}pakboycek
+ ${no++} ➳ ${prefix}pakgirlcek
+ ${no++} ➳ ${prefix}sangecek 
+ ${no++} ➳ ${prefix}bapercek
+ ${no++} ➳ ${prefix}fakboycek
+ ${no++} ➳ ${prefix}alimcek
+ ${no++} ➳ ${prefix}suhucek
+ ${no++} ➳ ${prefix}fakgirlcek
+ ${no++} ➳ ${prefix}kerencek
+ ${no++} ➳ ${prefix}wibucek
+
+ 𝘾𝙀𝙍𝙋𝙀𝙉 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}cerpen-sejarah
+ ${no++} ➳ ${prefix}cerpen-sedih
+ ${no++} ➳ ${prefix}cerpen-sastra
+ ${no++} ➳ ${prefix}cerpen-romantis
+ ${no++} ➳ ${prefix}cerpen-rohani
+ ${no++} ➳ ${prefix}cerpen-rindu
+ ${no++} ➳ ${prefix}cerpen-remaja
+ ${no++} ➳ ${prefix}cerpen-ramadhan
+ ${no++} ➳ ${prefix}cerpen-petualangan
+ ${no++} ➳ ${prefix}cerpen-persahabatan
+ ${no++} ➳ ${prefix}cerpen-perpisahan
+ ${no++} ➳ ${prefix}cerpen-perjuangan
+ ${no++} ➳ ${prefix}cerpen-penyesalan
+ ${no++} ➳ ${prefix}cerpen-pengorbanan
+ ${no++} ➳ ${prefix}cerpen-pengalaman
+ ${no++} ➳ ${prefix}cerpen-pendidikan
+ ${no++} ➳ ${prefix}cerpen-penantian
+ ${no++} ➳ ${prefix}cerpen-patahhati
+ ${no++} ➳ ${prefix}cerpen-olahraga
+ ${no++} ➳ ${prefix}cerpen-nasionalisme
+ ${no++} ➳ ${prefix}cerpen-nasihat
+ ${no++} ➳ ${prefix}cerpen-motivasi
+ ${no++} ➳ ${prefix}cerpen-misteri
+ ${no++} ➳ ${prefix}cerpen-mengharukan
+ ${no++} ➳ ${prefix}cerpen-malaysia
+ ${no++} ➳ ${prefix}cerpen-liburan
+ ${no++} ➳ ${prefix}cerpen-kristen
+ ${no++} ➳ ${prefix}cerpen-korea
+ ${no++} ➳ ${prefix}cerpen-kisahnyata
+ ${no++} ➳ ${prefix}cerpen-keluarga
+ ${no++} ➳ ${prefix}cerpen-kehidupan
+ ${no++} ➳ ${prefix}cerpen-jepang
+ ${no++} ➳ ${prefix}cerpen-inspiratif
+ ${no++} ➳ ${prefix}cerpen-gokil
+ ${no++} ➳ ${prefix}cerpen-galau
+ ${no++} ➳ ${prefix}cerpen-cintasejati
+ ${no++} ➳ ${prefix}cerpen-cintasegitiga
+ ${no++} ➳ ${prefix}cerpen-cintasedih
+ ${no++} ➳ ${prefix}cerpen-cintaromantis
+ ${no++} ➳ ${prefix}cerpen-cintapertama
+ ${no++} ➳ ${prefix}cerpen-cintaislami
+ ${no++} ➳ ${prefix}cerpen-cinta
+ ${no++} ➳ ${prefix}cerpen-budaya
+ ${no++} ➳ ${prefix}cerpen-bahasasunda
+ ${no++} ➳ ${prefix}cerpen-bahasajawa
+ ${no++} ➳ ${prefix}cerpen-bahasainggris
+ ${no++} ➳ ${prefix}cerpen-bahasadaerah
+ ${no++} ➳ ${prefix}cerpen-anak
+
+ `
+}
+
+exports.rulesBot = () =>{
+return`*──「 RULES-BOT 」──*
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+Jika sudah paham rulesnya
+Ketik *#menu* untuk memulai bot`
+}
+
+exports.donasiBot = (cekName,ucapanWaktu) =>{
+return`──「 *MENU DONATE* 」──
+
+Hi *${cekName}* ${ucapanWaktu} 👋🏻
+
+*Payment pulsa*
+Number: 083821115236
+A/N: __
+
+*Payment Dana*
+Number: 083821115236
+A/N: Ikbal Saepudin 
+
+${monoSpace}Terimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini ^_^${monoSpace}
+
+──「 *THX FOR YOU* 」──`
+}
+
+exports.infoOwner = () =>{
+return`──「 *INFO OWNER* 」──
+
+ *Data Profil*
+ • *Nama:* Ikbal Saepudin 
+ • *Umur:* 16 tahun
+ • *Hoby:* Turu/Ngoding/Mancing
+ • *Askot:* Majalengka 
+ • *Konten:* Blogger 
+
+_iam developer bot whatsapp._
+
+ *Sosial Media*
+ • *Whatsapp:* 083821115236
+ • *Youtube:* -
+ • *Github:* -
+ `
+}
